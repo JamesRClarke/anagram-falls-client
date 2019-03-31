90fce89a38231769a361a84c9d9076041bf75150
James Clarke <jamesraymondclarke@gmail.com>
2019-03-31 17:33:12 +0100

    create all pages as components and test for navigation links

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -6,7 +6,11 @@ import Aux from '../hoc/Aux'
 import Navigation from '../components/Navigation/Navigation'
 import SideNavigation from '../components/Navigation/SideNavigation'
 
+import Play from '../components/Play/Play'
+import Leaderboards from '../components/Leaderboards/Leaderboards'
 import About from '../components/About/About'
+import Register from '../components/Register/Register'
+import SignIn from '../components/SignIn/SignIn'
 
 
 class Layout extends Component {
@@ -27,7 +31,11 @@ class Layout extends Component {
           <Navigation clicked={this.toggleSideNavHandler} open={this.state.showSideNav}/>
           <SideNavigation open={this.state.showSideNav}/>
 
+          <Route path="/game" component={Play}/>
+          <Route path="/leaderboard" component={Leaderboards}/>
           <Route path="/about" component={About}/>
+          <Route path="/register" component={Register}/>
+          <Route path="/sign-in" component={SignIn}/>
 
         </Router>
       </Aux>
