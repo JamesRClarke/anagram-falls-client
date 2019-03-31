8f0b6ed9779f170edc4fbe07a2c826c6dacca89f
James Clarke <jamesraymondclarke@gmail.com>
2019-03-31 11:57:38 +0100

    add in the aux higher order component inside the hoc folder

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="d-flex justify-content-between m-5">
-          <p>is bootstrap in ?</p>
-          <p>is bootstrap in ?</p>
-          <p>is bootstrap in ?</p>
-        </div>
+      <div>
+
       </div>
     );
   }
