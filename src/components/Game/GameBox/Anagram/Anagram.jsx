7958655fe22a252f4c73d598228d7af21fad9a50
James Clarke <jamesraymondclarke@gmail.com>
2019-04-17 12:32:46 +0700

    switched the anagram animations to the anagram component so for each singular anagram we can seperate the logic

--- src/components/Game/GameBox/Anagram/Anagram.jsx
+++ src/components/Game/GameBox/Anagram/Anagram.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 
 
-const anagram = (props) => {
-  let anagramPosTop = props.top;
-  let anagramPosXY = props.xy;
-  console.log(props);
+class Anagram extends Component {
+  constructor(props) {
+    super(props);
+    state = {
+      anagramPosTop: -35,
+      anagramPosXY: createRandomXYPos()
+    }
+  }
+  
+  createRandomXYPos = () => {
+    return Math.ceil((Math.random() * (props.playingWidth - 80)));
+  }
+
+
+  let intervalId;
+  let moveAnagram = () => {
+    if(anagramPosTop <= props.playingHeight) {
+      return anagramPosTop = anagramPosTop + 5;
+
+    } else {
+      clearInterval(intervalId);
+    }
+  };
+  setInterval(moveAnagram, 500);
+
   return (
     <div >
-      <p className="game-anagram" style={{top: `${anagramPosTop}px`, left: `${anagramPosXY}px`}}>{props.anagram}</p>
+      <p
+        className="game-anagram"
+        style={{top: `${anagramPosTop}px`, left: `${anagramPosXY}px`}}>
+        {props.anagram}
+      </p>
     </div>
   )
 }
--- src/components/Game/GameBox/GameBox.jsx
+++ src/components/Game/GameBox/GameBox.jsx
@@ -4,53 +4,22 @@ import Aux from '../../../hoc/Aux';
 
 class GameBox extends Component {
   state = {
-    anagramPositionTop: -35,
     anagramPositionXY: null,
     gameBoxHeight: null,
     gameBoxWidth: null
   }
 
   componentDidMount() {
-
     const height = this.gameBox.clientHeight - this.inputBox.clientHeight - 35;
     const width = this.gameBox.clientWidth;
-
-    const randomStartPos = Math.ceil((Math.random() * (width - 80)));
     this.setState({
-      anagramPositionXY: randomStartPos,
       gameBoxHeight: height,
       gameBoxWidth: width
     })
-
   }
 
-  startIntervalHandler = () => {
-    let intervalId = null;
-    let moveAnagram = () => {
-      if(this.state.anagramPositionTop <= this.state.gameBoxHeight) {
-        this.setState(prevState =>  ({
-          anagramPositionTop: prevState.anagramPositionTop + 10
-        })
-      )
-    } else {
-      clearInterval(intervalId);
-    }
-  };
-
-  setInterval(moveAnagram, 500)
-
-}
-
-// do the setInterval inside the state of the gameBox and then p[ass through the props into the anagram component so the rerender will not matter.
 
 render() {
-
-  //  setInterval(() => {
-  //    this.setState({
-  //      anagramPositionTop: this.state.anagramPositionTop + 5
-  //    })
-  //    console.log('triggered');
-  // }, 2000)
 
   return (
     <Aux>
@@ -58,13 +27,11 @@ render() {
 
       <div ref={ (gameBox) => this.gameBox = gameBox} className="my-3 game-box">
 
-        <div ref={ (anagram) => this.anagram = anagram}>
-          <Anagram
-            anagram="anagram"
-            top={this.state.anagramPositionTop}
-            xy={this.state.anagramPositionXY}
-            />
-        </div>
+        <Anagram
+          anagram="Here I Am"
+          playingHeight={this.state.gameBoxHeight}
+          playingWidth={this.state.gameBoxWidth}
+          />
 
         <div ref={ (inputBox) => this.inputBox = inputBox} className="game-answer d-flex justify-content-center align-items-center">
           <input type="text"  />
